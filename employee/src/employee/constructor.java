ad66682b8101d8083a694617e58dba0fa5bed623
vimal <vimal@gmail.com>
2022-11-23 15:07:38 +0530

    commit

---
+++ employee/src/employee/constructor.java
@@ -0,0 +1,20 @@
+package employee;
+
+public class constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  constructor e1 = new constructor();
+   constructor e2 = new constructor();
+    
+     e1.display();
+     e2.display();
+     
+	}
+
+	private void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
